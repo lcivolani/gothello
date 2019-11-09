5ea8183bd0a18556f87c3c3b46e317dc44c61dca
Lorenzo Civolani <lcivolani@DESKTOP-6J79I35.lan>
2019-11-09 18:54:26 +0100

    diagonal directions

--- direction.go
+++ direction.go
@@ -10,8 +10,12 @@ func (d *Direction) Next(x, y int) (int, int) {
 
 // TODO: the order is not relevant: consider changing to a set
 var directions = []*Direction{
-	&Direction{-1, 0}, // up
-	&Direction{0, 1},  // right
-	&Direction{1, 0},  // down
-	&Direction{0, -1}, // left
+	&Direction{-1, 0},  // up
+	&Direction{-1, 1},  // up-right
+	&Direction{0, 1},   // right
+	&Direction{1, 1},   // down-right
+	&Direction{1, 0},   // down
+	&Direction{1, -1},  // down-left
+	&Direction{0, -1},  // left
+	&Direction{-1, -1}, // up-left
 }
--- main.go
+++ main.go
@@ -5,13 +5,29 @@ import (
 	"os"
 )
 
+var grid = `
+........
+........
+........
+..X.....
+...O...X
+....O.O.
+........
+........
+`
+
 func main() {
 	s := InitialState()
+	s.board = mustParseGrid(grid)
 	fmt.Print(s)
 	for i := 0; i < 5; i++ {
 		acts := s.Actions()
 		fmt.Println("actions:", acts)
-		fmt.Println("going to execute the first one...")
+		if len(acts) == 0 {
+			fmt.Println("no possible actions")
+			break
+		}
+		fmt.Println("going to execute the first one")
 		ns, err := s.Result(acts[0])
 		if err != nil {
 			fmt.Printf("error: %v\n", err)
