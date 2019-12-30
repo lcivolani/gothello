ed166aa1559229c3fee27a93d2d2a53c9eb6255e
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2019-12-30 01:31:14 +0100

    implemented alpha-beta pruning

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	p1 := &ComputerPlayer{Name: "Alice"}
-	p2 := &RandomPlayer{Name: "Bob"}
+	p2 := &ComputerPlayer{Name: "Bob"}
 
 	game := NewGame(p1, p2)
 	game.Play()
--- player.go
+++ player.go
@@ -73,39 +73,53 @@ func (p *ComputerPlayer) minimax(s *State) Action {
 	bestVal := math.MinInt32
 	for i := range s.Actions() {
 		ns, _ := s.Result(actions[i])
-		val := p.minValue(ns)
+		val := p.minValue(ns, math.MinInt32, math.MaxInt32)
 		fmt.Println("action", actions[i], "would lead to", val)
 		if val > bestVal {
 			best = i
 			bestVal = val
 		}
 	}
+	//time.Sleep(time.Duration(1) * time.Second)
+	fmt.Printf("%s picks %v thoughtfully from %v\n", p.Name, actions[best], actions)
 	return actions[best]
 }
 
-func (p *ComputerPlayer) minValue(s *State) int {
+func (p *ComputerPlayer) minValue(s *State, alpha, beta int) int {
 	if s.Terminal() {
 		return s.Utility(p.mark)
 	}
 	min := math.MaxInt32
 	for _, a := range s.Actions() {
 		ns, _ := s.Result(a)
-		if val := p.maxValue(ns); val < min {
+		if val := p.maxValue(ns, alpha, beta); val < min {
 			min = val
+		}
+		if min <= alpha {
+			return min
+		}
+		if min < beta {
+			beta = min
 		}
 	}
 	return min
 }
 
-func (p *ComputerPlayer) maxValue(s *State) int {
+func (p *ComputerPlayer) maxValue(s *State, alpha, beta int) int {
 	if s.Terminal() {
 		return s.Utility(p.mark)
 	}
 	max := math.MinInt32
 	for _, a := range s.Actions() {
 		ns, _ := s.Result(a)
-		if val := p.minValue(ns); val > max {
+		if val := p.minValue(ns, alpha, beta); val > max {
 			max = val
+		}
+		if max >= beta {
+			return max
+		}
+		if max > alpha {
+			alpha = max
 		}
 	}
 	return max
